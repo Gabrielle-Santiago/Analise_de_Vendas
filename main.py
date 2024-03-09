0e6644a9e508c9c822c1706b6ed226591430fcbd
Gabrielle Santiago Silva <gabriellesantisilva@gmail.com>
2024-03-09 20:34:41 -0300

    Criação das tabelas dos produtos

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 from os import listdir, path
-import pandas as pd
+from pandas import DataFrame, read_csv
+import plotly.express as px
 
 diretorio = r'C:\\Users\User\Downloads\Vendas'
 listaArquivos = listdir(diretorio)
+
+# Cria uma tabela vazia
+totalTabela = DataFrame()
 
 for arquivo in listaArquivos:
     if 'vendas' in arquivo.lower():
         #caminho do arquivo
         caminhoArq = path.join(diretorio, arquivo)
-        tabela = pd.read_csv(caminhoArq)
-        print(tabela)
-        
+        tabela = read_csv(caminhoArq)
+        totalTabela = totalTabela._append(tabela)
+
+# Separação da do produtos e a soma das quantidades vendidas
+tabelaProdutos = totalTabela.groupby('Produto').sum()[['Quantidade Vendida']]
+tabelaProdutos = tabelaProdutos.sort_values(by = 'Quantidade Vendida', ascending = False)
+
+# Visualização do produto que mais faturou
+totalTabela['Faturamento'] = totalTabela['Quantidade Vendida'] * totalTabela['Preco Unitario']
+fatTotal = totalTabela.groupby('Produto').sum()
+fatTotal = fatTotal[['Faturamento']].sort_values(by = 'Faturamento', ascending = False)
+
+# Visualização da loja que mais vendeu
+totalTabela['Loja'] = totalTabela['Loja']
+lojaTotal = totalTabela.groupby('Loja').sum()[['Faturamento']]
+
+# Dashboard
+lojaTotal = lojaTotal.reset_index()
+grafico = px.bar(lojaTotal, x = 'Loja' , y = 'Faturamento', title = 'Faturamento por loja')
+grafico.show()
+grafico.write_html('grafico.html')
